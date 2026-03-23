139a4ce1d4555a74263962dd8f7950ca924aa757
mkn-pcc <mknox@parklands.qld.edu.au>
2026-03-23 21:43:51 +1000

    Update avatars.js

--- illyria-read-the-room/data/avatars.js
+++ illyria-read-the-room/data/avatars.js
@@ -1,1 +1,26 @@
-
+const avatarsData = [
+    {
+        id: "male_1",
+        worldRead: "male",
+        full: "assets/portraits/player_male_1.png",
+        small: "assets/portraits/player_male_1_small.png"
+    },
+    {
+        id: "male_2",
+        worldRead: "male",
+        full: "assets/portraits/player_male_2.png",
+        small: "assets/portraits/player_male_2_small.png"
+    },
+    {
+        id: "female_1",
+        worldRead: "female",
+        full: "assets/portraits/player_female_1.png",
+        small: "assets/portraits/player_female_1_small.png"
+    },
+    {
+        id: "female_2",
+        worldRead: "female",
+        full: "assets/portraits/player_female_2.png",
+        small: "assets/portraits/player_female_2_small.png"
+    }
+];
