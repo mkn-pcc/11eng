e402ae69ee186878d19f0668055f0ed69d6dd0f0
mkn-pcc <mknox@parklands.qld.edu.au>
2026-03-23 21:50:23 +1000

    Update state.js

--- illyria-read-the-room/js/state.js
+++ illyria-read-the-room/js/state.js
@@ -1,1 +1,17 @@
+let gameState = {
+    avatar: null,
+    sceneIndex: 0,
+    c: gameConfig.startStats.c,
+    a: gameConfig.startStats.a,
+    p: gameConfig.startStats.p
+};
 
+function resetState() {
+    gameState = {
+        avatar: null,
+        sceneIndex: 0,
+        c: gameConfig.startStats.c,
+        a: gameConfig.startStats.a,
+        p: gameConfig.startStats.p
+    };
+}
