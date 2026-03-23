92db7faaa6e34641d1ce69c8428628de6f308c70
mkn-pcc <mknox@parklands.qld.edu.au>
2026-03-23 21:50:09 +1000

    Update sceneManager.js

--- illyria-read-the-room/js/sceneManager.js
+++ illyria-read-the-room/js/sceneManager.js
@@ -1,1 +1,41 @@
+function selectAvatar(avatar) {
+    gameState.avatar = avatar;
+    stopAudio(); // Music stops during gameplay
+    showScreen("screen-game");
+    updateHUD();
+    loadScene();
+}
 
+function loadScene() {
+    if (gameState.sceneIndex >= scenesData.length) {
+        triggerEnding();
+        return;
+    }
+    const scene = scenesData[gameState.sceneIndex];
+    renderScene(scene);
+}
+
+function handleChoice(choiceObj, npc) {
+    // Apply hidden variable effects
+    gameState.c = clampStat(gameState.c + choiceObj.effects.c);
+    gameState.a = clampStat(gameState.a + choiceObj.effects.a);
+    gameState.p = clampStat(gameState.p + choiceObj.effects.p);
+    
+    updateHUD();
+
+    // Get reaction based on worldRead (male/female avatar)
+    const reaction = choiceObj.reaction[gameState.avatar.worldRead];
+    renderReaction(reaction, npc);
+}
+
+function advanceScene() {
+    gameState.sceneIndex++;
+    loadScene();
+}
+
+function triggerEnding() {
+    const ending = calculateEnding(gameState.c, gameState.a, gameState.p);
+    renderEnding(ending);
+    showScreen("screen-ending");
+    playAudio(); // Music resumes on ending screen
+}
