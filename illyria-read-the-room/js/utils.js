b9a446648da940ec348d3172b2d331e8a27c27c9
mkn-pcc <mknox@parklands.qld.edu.au>
2026-03-23 21:50:39 +1000

    Update utils.js

--- illyria-read-the-room/js/utils.js
+++ illyria-read-the-room/js/utils.js
@@ -1,1 +1,7 @@
+function clampStat(val) {
+    return Math.max(gameConfig.minStat, Math.min(gameConfig.maxStat, val));
+}
 
+function getSocialHealth() {
+    return Math.round((gameState.c + gameState.a + gameState.p) / 3);
+}
