5389a801203e0df812552359f59c2fddca69e079
mkn-pcc <mknox@parklands.qld.edu.au>
2026-03-23 21:49:32 +1000

    Update endings.js

--- illyria-read-the-room/js/endings.js
+++ illyria-read-the-room/js/endings.js
@@ -1,1 +1,8 @@
-
+function calculateEnding(c, a, p) {
+    for (let ending of endingsData) {
+        if (ending.condition(c, a, p)) {
+            return ending;
+        }
+    }
+    return endingsData[endingsData.length - 1]; // Safe fallback
+}
