c0cbdea489749af61d152ee7537791197919947d
mkn-pcc <mknox@parklands.qld.edu.au>
2026-03-23 21:48:04 +1000

    Update characters.js

--- illyria-read-the-room/data/characters.js
+++ illyria-read-the-room/data/characters.js
@@ -1,1 +1,10 @@
-
+const charactersData = {
+    duke: { name: "Duke", basePath: "assets/portraits/duke" },
+    monique: { name: "Monique", basePath: "assets/portraits/monique" },
+    olivia: { name: "Olivia", basePath: "assets/portraits/olivia" },
+    coach: { name: "Coach Dinklage", basePath: "assets/portraits/coach" },
+    teacher: { name: "Miss Stratton", basePath: "assets/portraits/teacher" },
+    outsider: { name: "Eunice", basePath: "assets/portraits/outsider" },
+    gossip: { name: "Kevin", basePath: "assets/portraits/gossip" },
+    chaperone: { name: "Mrs Ripley", basePath: "assets/portraits/chaperone" }
+};
